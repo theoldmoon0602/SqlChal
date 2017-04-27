d78ef5b30a767b252659f785fcecd0462fc2c5e7
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-04-28 08:25:26 +0900

    update

--- problems/problem6.php
+++ problems/problem6.php
@@ -6,6 +6,6 @@ return [
 'text' => '相互フォローの関係にあるユーザの組み合わせをすべて表示してください。ただし、<code>from_id</code>と<code>to_id</code>に昇順になるようにソートしてください。<code>from_id</code>のほうがソートの優先順位は上です',
 'name' => 'おともだち',
 'point' => 100,
-'answer_query' => 'from_id, to_id from followings as a inner join followings as b on a.from_id=b.to_id and a.to_id=b.from_id order by from_id, to_id',
-'sample' => 'from_id from followings where to_id=1024 and from_id in (select to_id from followings where from_id=1024) order by from_id;',
+'answer_query' => 'from_id, to_id from followings as a inner join followings as b on a.from_id=b.to_id and a.to_id=b.from_id order by from_id, to_id;',
+'sample' => 'from_id, to_id from followings as a inner join followings as b on a.from_id=b.to_id and a.to_id=b.from_id order by from_id, to_id;',
 ];
