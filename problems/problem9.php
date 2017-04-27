d5830f2eb18b60d3c80c9fed97b48f8239928484
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-04-27 22:18:32 +0900

    fix problems

--- problems/problem9.php
+++ problems/problem9.php
@@ -4,11 +4,10 @@ return [
     'story' => 'kyumina くんは大変な事に気が付きました。 KosenProconTwitter にはまだ、タイムラインの機能が存在しないのです。
 タイムラインとは、あるユーザがフォローしている人のツイートが、時系列順に並んだものです。Twitterを名乗るからには必須の機能でした。
 これは致命的なミスです。 kyumina くんは急いで実装を始めることにしました。そこで、あなたに、タイムラインを取得するようなクエリを作るように依頼してきました。
-依頼の内容は、ユーザ id が 20 の人にフォローされているユーザのツイートを、時刻に降順に並べて取得することです。 
-サンプルでは、ユーザidが2の人について、これを実行しています。',
-    'text' => 'ユーザidが20の人のタイムライン（そのユーザにフォローされているユーザのツイートを時刻（ <code>created_at</code> ）に昇順に並べたもの）を表示してください。',
+依頼の内容は、ユーザ id が 20 の人にフォローされているユーザのツイートを、時刻に降順に並べて取得することです。',
+    'text' => '<code>id</code>が51066であるユーザのタイムライン（そのユーザにフォローされているユーザのツイートを時刻（ <code>created_at</code> ）に昇順に並べたもの）を表示してください。',
     'name' => 'kyumina くんとタイムライン',
     'point' => 250,
-    'answer_query' => ' content from tweets where user_id in (select to_id from followings where from_id=20) order by created_at;',
-    'sample' => ' content from tweets where user_id in (select to_id from followings where from_id=2) order by created_at;',
+    'answer_query' => ' content from tweets where user_id in (select to_id from followings where from_id=51066) order by created_at;',
+    'sample' => ' content from tweets where user_id in (select to_id from followings where from_id=51066) order by created_at;',
 ];
