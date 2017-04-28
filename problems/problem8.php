d4fe2e09d22871c33f0ae93e7f6f01f4e02e2b2a
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-04-28 22:28:38 +0900

    update

--- problems/problem8.php
+++ problems/problem8.php
@@ -3,9 +3,9 @@
 return [
     'story' => 'ユーザのレコメンド機能の実装を考えていた kyumina くんが、あなたに「あるユーザについて、そのユーザがフォローしているユーザがフォローしている」ようなユーザを取得したいとお願いしてきました。
 これはさほど難しいタスクではないだろうと考えたあなたは、さっそくこの仕事にとりかかりました。',
-    'text' => 'ユーザidが14283のユーザにフォローされているユーザがフォローしているユーザのユーザidを昇順で、重複がないように取得してください。',
+    'text' => 'ユーザidが4070のユーザにフォローされているユーザがフォローしているユーザのユーザidを昇順で、重複がないように取得してください。',
     'name' => 'user recommendation',
     'point' => 250,
-    'answer_query' => 'to_id from followings where from_id in (select to_id from followings where from_id=14283) order by to_id;',
-    'sample' => 'to_id from followings where from_id in (select to_id from followings where from_id=14460) order by to_id;',
+    'answer_query' => 'to_id from followings where from_id in (select to_id from followings where from_id=4070) order by to_id;',
+    'sample' => 'to_id from followings where from_id in (select to_id from followings where from_id=4070) order by to_id;',
 ];
