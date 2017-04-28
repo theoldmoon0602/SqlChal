4647b49ba0b1945a676a2e428142180ed285c3cd
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-04-28 22:48:18 +0900

    update

--- problems/problem4_4.php
+++ problems/problem4_4.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'story' => 'さて、KosenProconTwitterに向けられる当然の需要として、ユーザ名とツイートを一度に表示したい、というものがあります。これは基礎のクエリですが、複数のテーブルを結合しなければ実現できないため、簡単な問題ではありません。
+    'story' => 'さて、KosenProconTwitterに向けられる当然の需要として、ユーザ名とツイートを同時に表示したい、というものがあります。これは基礎のクエリですが、複数のテーブルを結合しなければ実現できないため、簡単な問題ではありません。
 inner join 句をうまく使って、これを実現してみましょう。',
-    'text' => 'ツイートをidに昇順に100件、その発言者の <code>username</code> とともに表示してください。',
+    'text' => 'ツイートの内容<code>tweets.content</code>とその発言者のユーザ名<code>users.username</code> を、ツイートのid <code>tweets.id</code> について昇順に100件表示してください。',
     'point' => 150,
     'name' => '基本のキ',
     'answer_query' => 'username, content from tweets inner join users on users.id = tweets.user_id order by tweets.id asc limit 100',
