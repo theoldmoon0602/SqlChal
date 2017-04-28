e823c36f77e657d88621b9dd85e6e948cd10c8ec
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-04-29 00:23:24 +0900

    update

--- problems/problem7.php
+++ problems/problem7.php
@@ -2,7 +2,7 @@
 
 return [
     'story' => 'pawoo.netがpixiv社の運営のもとにあるのは周知の事実ですが、それでは @pixiv にはどのくらいの数のリプライが飛んでいるのでしょうか。運営の負荷を見積もるためにはこの値は参考になりそうです。',
-    'text' => '<code>@pixiv </code> が含まれるようなツイートの内容 <code>tweets.content</code>を、そのid <code>tweets.id</code> に昇順に表示してください。',
+    'text' => '<code>@pixiv&nbsp;</code> が含まれるようなツイートの内容 <code>tweets.content</code>を、そのid <code>tweets.id</code> に昇順に表示してください。',
     'name' => '運営へのリプライ',
     'point' => 140,
     'answer_query' => ' content from tweets where content like \'%@pixiv %\' order by id;',
